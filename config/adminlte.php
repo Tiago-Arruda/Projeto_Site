24af0e4f0145e3be6b8b0db96a087a463080bbe6
gesserpaixao <31105956+gesserpaixao@users.noreply.github.com>
2020-04-06 21:59:22 -0400

    Update adminlte.php

--- config/adminlte.php
+++ config/adminlte.php
@@ -178,6 +178,8 @@ return [
     'password_reset_url' => 'password/reset',
 
     'password_email_url' => 'password/email',
+    
+    'upload' => 'upload',
 
     'profile_url' => false,
 
@@ -212,6 +214,7 @@ return [
             'text' => 'search',
             'search' => true,
             'topnav' => true,
+            
         ],
         [
             'text' => 'blog',
@@ -222,72 +225,46 @@ return [
             'text'        => 'pages',
             'url'         => 'admin/pages',
             'icon'        => 'far fa-fw fa-file',
+            'icon_color' => 'info',
             'label'       => 4,
             'label_color' => 'success',
         ],
-        ['header' => 'account_settings'],
+        [
+            'text'        => 'Upload Arquivo',
+            'url'         => 'docs/upload',
+            'icon'        => 'far fa-fw fa-file',
+            'icon_color' => 'danger',
+            'label'       => 'Insert',
+            'label_color' => 'success',
+            
+        ],
+        [
+            'text'        => 'Arquivos',
+            'url'         => 'usuarios/Download',
+            'icon_color' => 'warning',
+            'icon'        => 'far fa-fw fa-file',
+            'label'       => 'Baixar',
+            'label_color' => 'primary',
+        ],
+
+
+        ['header' => 'Administrador'],
         [
             'text' => 'profile',
             'url'  => 'admin/settings',
-            'icon' => 'fas fa-fw fa-user',
+            'icon' => 'fas fa-fw fa-user',            
         ],
         [
             'text' => 'change_password',
             'url'  => 'admin/settings',
             'icon' => 'fas fa-fw fa-lock',
+            
         ],
-        [
-            'text'    => 'multilevel',
-            'icon'    => 'fas fa-fw fa-share',
-            'submenu' => [
-                [
-                    'text' => 'level_one',
-                    'url'  => '#',
-                ],
-                [
-                    'text'    => 'level_one',
-                    'url'     => '#',
-                    'submenu' => [
-                        [
-                            'text' => 'level_two',
-                            'url'  => '#',
-                        ],
-                        [
-                            'text'    => 'level_two',
-                            'url'     => '#',
-                            'submenu' => [
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-                [
-                    'text' => 'level_one',
-                    'url'  => '#',
-                ],
-            ],
-        ],
-        ['header' => 'labels'],
-        [
-            'text'       => 'important',
-            'icon_color' => 'red',
-        ],
-        [
-            'text'       => 'warning',
-            'icon_color' => 'yellow',
-        ],
-        [
-            'text'       => 'information',
-            'icon_color' => 'aqua',
-        ],
+                
+        ['header' => '@gesserpaixao @Tiagoarruda'],
+
     ],
+    
 
     /*
     |--------------------------------------------------------------------------
