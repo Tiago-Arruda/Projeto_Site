b7e781a618b369c86ea15d8860b3b4fcf5fb32f7
gesserpaixao <31105956+gesserpaixao@users.noreply.github.com>
2020-04-07 01:22:11 -0400

    Update upload.blade.php

--- resources/views/admin/Upload/upload.blade.php
+++ resources/views/admin/Upload/upload.blade.php
@@ -1,50 +1,94 @@
 @extends('adminlte::page')
 
-@section('title', 'AdminLTE')
+@section('title', 'Upload')
+
+@section('content_header')
+<h1>Cadastro de Arquivos</h1>
+
+    <ol class="breadcrumb">
+        <li><a href="">Home&nbsp;|&nbsp;</a><li>
+        <li><a href="">Administrador&nbsp;|&nbsp;</a><li >        
+        <li><a href="">Upload</a><li >        
+    </ol>
+@stop
 
 @section('content_header')
     <h1 class="m-0 text-dark">Upload de Documentos</h1>
 @stop
 
 @section('content')
-    <div class="row">
-        <div class="col-12">
-            <div class="card">
-                <div class="card-body">
-                    <p class="mb-0">UpLoad de Arquivos</p>
+  
+    <div>
+
+    <form method="post" class="form-inline" action="{{route('admin.Upload.upload')}}" enctype="multipart/form-data">    
+        {{ csrf_field() }} 
+        <div class="row form-inline">
+            
+                <div class="input-group margin ">
+                    <label for="inputEmail3" class="col-sm-6 col-form-label">Procurar por nome</label>
+                    <input type="text" name="_nome"  placeholder="Digite o nome" class="form-control ol-xs-4 input-sm" />
+                            <span class="input-group-btn">
+                                <button type="submit" class="btn btn-info btn-flat" nome="btn-busca">Procurar  <span class="glyphicon glyphicon-search">
+                                </span>
+                                </button>
+                            </span>
+                </div>
+            
+            <div class="input-group margin ">
+                <label for="inputEmail3" class="col-sm-6 col-form-label">Procurar por conteudo</label>
+                    <input type="text" name="_conteudo"  placeholder="#" class="form-control " />
+                            <span class="input-group-btn">
+                                <button type="submit" class="btn btn-info btn-flat" nome="btn-busca">Procurar  <span class="glyphicon glyphicon-search">
+                                </span>
+                                </button>
+                            </span>
                 </div>
             </div>
         </div>
-    </div>
-    <div>
-        <form method="post" action="uploads" enctype="multipart/form-data">
+    </form>
+    <p>
+
+
+<!-- /Cadastrar Arquivo-->
+
+        <form method="post"   placeholder="R"
+            action="{{route('admin.Upload.upload')}}" enctype="multipart/form-data">
             {{ csrf_field() }}
-            <div class="form-row">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card bg-success">
+                        
+                            <p class="mb-0">UpLoad de Arquivos</p>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="form-row col-md-15">
                 <div class="form-group col-md-4">
-                <label for="inputEmail3" class="col-sm-4 col-form-label">Nome</label>
-                <input type='text' class="form-control" id="nome" name="nome"  />  
-                <label for="inputEmail3" class="col-sm-4 col-form-label">Industria</label>          
-                <input type='text' class="form-control" id="industria" name="industria"  />            
+                    <label for="inputEmail3" class="col-sm-4 col-form-label">Nome</label>
+                    <input type='text' class="form-control" id="nome" name="nome"  placeholder="Nome do Arquivo"  />  
+                    <label for="inputEmail3" class="col-sm-4 col-form-label "  placeholder="Nome da Industria">Industria</label>          
+                    <input type='text' class="form-control" id="industria" name="industria"  />            
                 </div>
                 
+                <div class="form-group col-md-4">
+                    <label for="inputEmail3" class="col-sm-4 col-form-label">Validade</label>
+                    <input type='date' class="form-control" id="Validadedoc" name="Validadedoc"  />
+                    <label for="inputEmail3" class="col-sm-4 col-form-label" >Conteudo</label>
+                    <input type='text' class="form-control"  id="conteudo" name="conteudo"  placeholder="Descreva o conteudo espaçando com #" />
+                
+                </div>
                 <div class="form-group col-md-2">
-                <label for="inputEmail3" class="col-sm-4 col-form-label">Validade</label>
-                <input type='text' class="form-control" id="Validadedoc" name="Validadedoc"  />
-                <label for="inputEmail3" class="col-sm-4 col-form-label">Conteudo</label>
-                <input type='text' class="form-control"  id="conteudo" name="conteudo" />
-                
-                </div>
-                <div class="form-group col-md-3">
-                    <label for="inputEmail3" class="col-sm-6 col-form-label">Estado do Doc.</label>
+                    <label for="inputEmail3" class="col-sm-7 col-form-label">Situação</label>
                     <select type='text' class="form-control"  id="estado" name="estado">
                             <option value="0" disabled selected>Ativo</option>
                             <option value="1">Inativo</option>
                             <option value="2">Cancelado</option>                            
                     </select>
-                    <label for="inputEmail3" class="col-sm-4 col-form-label">Versão</label>
+                    <label for="inputEmail3" class="col-sm-3 col-form-label">Versão</label>
                     <input type='text' class="form-control" id="versao" name="versao"  />     
                 </div>
-                <div class="form-group col-md-6">                                  
+                <div class="form-group col-md-10">                                  
                     <label for="inputEmail3" class="col-sm-4 col-form-label">Arquivos</label>
                     <input type='file' class="form-control" id="primaryImage" name="primaryImage" accept="image/*" />
                 </div>
