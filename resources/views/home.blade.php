558ad67b8e786d5ec5a1eb4440672166432158ae
gesserpaixao <31105956+gesserpaixao@users.noreply.github.com>
2020-04-06 22:32:19 -0400

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 @extends('adminlte::page')
 
 @section('title', 'AdminLTE')
@@ -17,3 +18,24 @@
         </div>
     </div>
 @stop
+=======
+@extends('adminlte::page')
+
+@section('title', 'AdminLTE')
+
+@section('content_header')
+    <h1 class="m-0 text-dark">Home</h1>
+@stop
+
+@section('content')
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <p class="mb-0">Seja Bem Vindo!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
+>>>>>>> 9f2c1925dc5376725cc7027b2cc48a6af6ce5d46
