e36036f552b8511faab6bfa764f5f161f12c4238
gesserpaixao <31105956+gesserpaixao@users.noreply.github.com>
2020-04-10 16:45:28 -0400

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,9 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('industria')->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password');            
             $table->boolean('isadmin')-> default(false);
             $table->rememberToken();
             $table->timestamps();
