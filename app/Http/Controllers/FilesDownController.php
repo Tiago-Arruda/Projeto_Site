2ab031ddd34d9cac2d6f668f01ebf0d157d54420
gesserpaixao <31105956+gesserpaixao@users.noreply.github.com>
2020-04-11 21:01:14 -0400

    Update FilesDownController.php

--- app/Http/Controllers/FilesDownController.php
+++ app/Http/Controllers/FilesDownController.php
@@ -11,6 +11,9 @@ use Illuminate\Support\Facades\Auth;
 use App\User;
 use Illuminate\Support\Facades\Storage;
 
+use App\Http\Controllers\Controller;
+
+
 class FilesDownController extends Controller
 {
     /**
@@ -21,9 +24,78 @@ class FilesDownController extends Controller
     */
     public function index()
     {
-     
+        
+        $industriaUsers = Auth::user()->industria; 
+                       
+        if (is_null($industriaUsers)){                        
+            return redirect()
+            ->route('users.downloads.down')
+            ->with('error', 'Verifique com o Administrador'); 
+        }
+        else
+        {    
+            $file = DB::table('files')
+            ->where('industria', 'like',  "%" . $industriaUsers)
+            ->get();                    
+        }
+                     
+        if (!empty($file['0']->id)){                        
+            $id = $file['0']->id;
+            return 
+                view('users.downloads.down')->with('file', $file)->with('id',$id);                            
+            }else{
+                return redirect()
+                    ->route('home')
+                    ->with('error', 'Registro não encontrado');
+            }
+
+
+        
+        //return view('users.downloads.down');
         /// fazer o filtro por industria                                
-    }    
+    }   
+    
+/**
+    * Resolve o envio do arquivo.
+    *
+    * @param Request $request A instância do request.
+    * @return Response A instância da response.
+    */
+   
+        public function show(Request $request)
+        {
+            $_idfile = $request->input('idfile');   
+
+            if(empty(_idfile)){
+                $file = DB::table('files')        
+                ->where('id', 'like',  "%" . $_idfile)
+                ->get();
+
+            }else
+            {
+                return redirect()
+                ->route('home')
+                ->with('error', 'Registro não encontrado');
+            }
+
+            $arq = $file['0']->caminho;
+            $arqt = $file['0']->tipo;
+            //$arqb = storage_path('app/'.$arq+"."+$arqt);
+
+            $exists = Storage::disk('app/')->exists($arq);
+
+            $arqb = storage_path('app/'.$arq);
+            return response()->download($arqb);
+                        //return response()->download($arqb);
+                        //return Storage::download('file.jpg', $name, $headers);
+
+           // return Storage::download($arqb->path, $arqb->name);
+        }
+
+
+
+
+
 
    //fim da controller 
 
