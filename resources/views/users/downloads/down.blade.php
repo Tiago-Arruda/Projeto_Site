93754e20e9a01ec7dffd06ff1c79ddb3773da867
gesserpaixao <31105956+gesserpaixao@users.noreply.github.com>
2020-04-11 21:01:20 -0400

    Update down.blade.php

--- resources/views/users/downloads/down.blade.php
+++ resources/views/users/downloads/down.blade.php
@@ -47,8 +47,10 @@
                             <th>Conteudo</th>
                             <th>Industria</th>
                             <th>Validade</th>
-                            <th>Versão</th>
+                            <th>Versão</th>                                                   
+                            <th>Imagem</th>
                             <th>Ação</th>
+                            
                             
                             
                         </tr>
@@ -56,19 +58,34 @@
                     <tbody>
                         @forelse($file as $Item)
                             <tr>
-                                <td style="display:none;" id="{{$Item->id}}" value="{{$Item->id}}">{{$Item->id}}</td>
+                                <td style="display:none;" id="{{$Item->id}}" value="{{$Item->id}}">
+                                    {{$Item->id}}</td>
                                 <td>{{$Item->name}}</td>
                                 <td>{{$Item->estado}}</td>
                                 <td>{{$Item->conteudo}}</td>
                                 <td>{{$Item->industria}}</td>
                                 <td>{{$Item->Validadedoc}}</td>
-                                <td>{{$Item->versao}}</td>                                
+                                <td>{{$Item->versao}}</td>                                                                
                                 <td>
-                                                              
-                                    
+                                <img class="card-img-top img-responsive"  src="{{Storage::url($Item->caminho)}}.jpg" />
+                                </td>   
+                                <td>
+                                    <form style="display:inline; width:100px;" method="post" 
+                                    action="{{route('users.downloads.baixar')}}">
+                                        {!! csrf_field()!!}
+                                            <input style="display:none;"  type="text" name="_idfile" value="{{$file[0]->id}}" 
+                                            class="form-control">
+                                            <span class="input-group-btn">                                        
+                                                <button type="button" class="btn btn-success" name="btn-b" ><span class="fa fa-download">
+                                                </span>
+                                            </button>
+                                            <button type="button" class="btn btn-info" name="btn-v"><span class="fa fa-eye">
+                                            </span></button>
+                                    </span>
+                                    </form>                                                                                           
                                 </td>
                             </tr>                             
-                        @empty
+        @empty
                         @endforelse                         
                     </tbody>
                 </table>                
