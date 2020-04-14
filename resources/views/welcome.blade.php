27c330067134c151fb0aaf51475fd90c5ae5d0c1
gesserpaixao <31105956+gesserpaixao@users.noreply.github.com>
2020-04-14 10:24:13 -0400

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,13 +17,38 @@
         <link rel="stylesheet" href="../dist/css/adminlte.min.css">
         <!-- Google Font: Source Sans Pro -->
         <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
-        
+
+
+        <link rel="apple-touch-icon" sizes="57x57" href="ico/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="ico/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="ico/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="ico/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="ico/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="ico/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="ico/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="ico/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="ico/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"  href="ico/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="ico/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="ico/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="ico/favicon-16x16.png">
+        <link rel="manifest" href="/manifest.json">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+        <meta name="theme-color" content="#ffffff">
+
+
+
+
+
+
+
         <!-- Styles -->
         <style>
             
         </style>
     </head>
-    <body style="background-image: url('../img/logo_site.png'); 
+    <body style="background-image: url('../img/home3.jpg'); 
         background-repeat: no-repeat, repeat; background-size: cover; opacity: 0.9;">
         
         <nav class="navbar navbar-inverse">
@@ -36,7 +61,7 @@
                             <span class="icon-bar"></span>
                         </button>
                         <a class="navbar-brand" href="#">
-                        <img src="../img/corteva.png" width="30" heigth="30"/>                        
+                        <img src="../img/logo_site.png" width="30" heigth="30"/>                        
                         </a>
                     </div>
                     <div class="navbar-header">
