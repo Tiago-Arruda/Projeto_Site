9b0609943ee7698116231eff4d38a8d97fa0a4de
gesserpaixao <31105956+gesserpaixao@users.noreply.github.com>
2020-04-13 23:10:56 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -30,18 +30,23 @@ Route::group(['middleware' => ['auth','admin.auth'],'namespace' => 'Admin', 'pre
     //usuarios
     Route::get('cadastro','CadastroController@index')->name('admin.cadastro.cadindex');
     Route::post('cadastro','CadastroController@buscar')->name('admin.cadastro.cadindex');
-    
+         
+    //Route:get('cadastro','CadastroController@index')->name('admin.cadastro.cadindex');
+    //$this->post('cadastro','CadastroController@buscar')->name('admin.cadastro.cadindex');
+    Route::any('cadastroup','CadastroController@atualiza')->name('cadastroup');
+    Route::any('cadastrodown','CadastroController@excluir')->name('cadastrodown');
+
     //arquivos @gesser Miguel
     Route::get('upload','FilesController@index')->name('admin.Upload.upload');
     Route::post('upload','FilesController@uploads')->name('admin.Upload.upload');
     Route::get('busca','FilesController@buscar')->name('admin.Upload.busca');
     Route::any('search','FilesController@search')->name('search');
     Route::any('uploadatualiza','FilesController@atualizadoc')->name('uploadatualiza');
-         
-    //Route:get('cadastro','CadastroController@index')->name('admin.cadastro.cadindex');
-    //$this->post('cadastro','CadastroController@buscar')->name('admin.cadastro.cadindex');
-    Route::any('cadastroup','CadastroController@atualiza')->name('cadastroup');
-    Route::any('cadastrodown','CadastroController@excluir')->name('cadastrodown');
+
+    //arquivos @gesser Miguel
+    Route::get('clientes','ClientesController@index')->name('admin.clientes.clientes');
+    Route::any('pesquisa','ClientesController@pesquisa')->name('pesquisa');
+    Route::post('clientesup','ClientesController@iserir')->name('clientesup');
 
 });
 
