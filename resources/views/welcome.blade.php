cb6a5b67a105315333bc79b0ef16271514fc74cc
gesserpaixao <31105956+gesserpaixao@users.noreply.github.com>
2020-04-14 23:16:15 -0400

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -48,10 +48,107 @@
                     height: 400px;
                     width: 230px;
                     position: absolute;
-                        top: 90px;
+                        top: 55px;
                         right: 20px;
-                }
-            
+                }       
+
+
+            .box-widget {
+                border: none;
+                position: relative;
+            }
+            .box {
+                position: relative;
+                border-radius: 3px;
+                background: #ffffff;
+                border-top: 3px solid #d2d6de;
+                margin-bottom: 20px;
+                width: 100%;
+                box-shadow: 0 1px 1px rgba(0,0,0,0.1);
+            }
+            .widget-user .widget-user-header {
+                padding: 20px;
+                height: 120px;
+                border-top-right-radius: 3px;
+                border-top-left-radius: 3px;
+            }
+            .bg-yellow {
+                background-color: #f39c12 !important;
+            }
+            .bg-blue {
+                background-color: #0073b7 !important;
+            }
+            .bg-aqua {
+                background-color: #00c0ef !important;
+            }
+            .bg-green {
+                background-color: #00a65a !important;
+            }
+            .bg-red {
+                background-color: #dd4b39 !important;
+            }
+            .widget-user .widget-user-username {
+                margin-top: 0;
+                margin-bottom: 5px;
+                font-size: 25px;
+                font-weight: 300;
+                text-shadow: 0 1px 1px rgba(0,0,0,0.2);
+                color:#fff;
+            }
+            .widget-user .widget-user-desc {
+                margin-top: 0;
+                color:#fff;
+            }
+
+            .widget-user .widget-user-image {
+                position: absolute;
+                top: 30px;
+                left: 50%;
+                margin-left: -45px;
+            }
+            .widget-user .widget-user-image2 {
+                position: absolute;
+                top: 50px;
+                left: 45%;
+                margin-left: -45px;
+            }
+            .widget-user .widget-user-image>img {
+                width: 90px;
+                height: auto;
+                border: 5px solid #fff;
+            }
+            .widget-user .box-footer {
+                padding-top: 30px;
+            }
+            .box-footer {
+                border-top-left-radius: 0;
+                border-top-right-radius: 0;
+                border-bottom-right-radius: 3px;
+                border-bottom-left-radius: 3px;
+                border-top: 1px solid #f4f4f4;
+                padding: 10px;
+                background-color: #fff;
+            }
+            .box .border-right {
+                border-right: 1px solid #f4f4f4;
+            }
+            .description-block {
+                display: block;
+                margin: 10px 0;
+                text-align: center;
+            }
+            .description-block>.description-header {
+                margin: 0;
+                padding: 0;
+                font-weight: 600;
+                font-size: 16px;
+            }
+            .description-block>.description-text {
+                text-transform: uppercase;
+            }
+
+
+
         </style>
     </head>
     <body class="hold-transition sidebar-mini" style="background-image: url('../img/sla_logo3.png'); 
@@ -96,7 +193,6 @@
 @stop
 
 <div class="container">
-
 <div class="row">
     <div class="col-sm-3">
 
@@ -104,50 +200,82 @@
     <div class="col-sm-3">
                               
       </div>
-    <div class="col-sm-5">
-                        <div class="card bg-info">                        
-                            <div class="card-body">                                        
-                                <h2 class="card-title text-center display-4">
-                                    <b>SOLO CERTO <i> Agroambiental</i></b>
-                                </h2>
-                                <div class="card-body text-center">
-                                    <img src="\img\sla_logo.png" class="card-img-top text-center" alt="...">
-                                </div>                        
-                                <div class="card-body">
-                                <h5 class="description-header">Endereço:</h5>
-                                    <p class="card-text">
-                                        Av. Brasil nº 269, Torre H Bloco 4 AP. 1002 <br/>
-                                        Morada do Ouro II  Cep: 78.053 – 734 Cuiabá/MT<br/>
-                                        Contato: francopasa26@gmail.com<br/>                                    
-                                    </p>
-                                </div>                        
-                                <div class="card-body">
-                                    <p class="card-text">
-                                        <i>Se  projetas  alguma  coisa,  ela  te  sairá  bem,  e  a luz brilhará  em  
-                                        teus  caminhos. (Jó 22:28)</i>
-                                    </p>
-                                </div>                                                               
-                            </div>
-                        </div>
-                        <div class="card bg-danger">                        
-                            <div class="card-body">                                        
-                                <div class="widget-user-header ">                                
-                                    <h1 class="widget-user-username"><b>Franco Pasa</b></h1>
-                                        <h5 ><b><i>CEO - Agronomo Responsável</i></b></h5>
-                                 </div>
-                                <div class="card-body text-center elevation-6">
-                                    <img src="\img\avatar1.jpg" 
-                                        class="card-img-top text-center elevation-3" />
-                                </div>                        
-                                <div class="description-block">                                   
-                                        <p class="card-text">                                    
-                                        FRANCO MARCELO PASA HERNANDEZ<br/>                                        
-                                        EMAIL: pasahernandez@yahoo.com.br  - Tel: (65) 9614 – 2632<br/>
-                                          
+    <div class="col-sm-5">                        
+                    <div class="card bg-yellow">   
+                        <div class="col-md-16">
+                            <div class="box box-widget widget-user">
+                                <div class="widget-user-header bg-yellow">
+                                <h3 class="widget-user-username">
+                                    <b>SOLO CERTO <i> Agroambiental</i></b>    
+                                </h3>                                
+                                </div>
+                                <div class="widget-user-image2" style="">
+                                     <img src="\img\sla_logo.png" class="card-img-top text-center" alt="...">
+                                </div>
+                                <div class="box-footer">
+                                <div class="row">
+                                    <div class="col-sm-8 border-right">
+                                    <div class="description-block">
+                                        <p class="card-text text-left">
+                                            Av. Brasil nº 269, Torre H Bloco 4 AP. 1002 <br/>
+                                            Morada do Ouro II  Cep: 78.053 – 734 Cuiabá/MT<br/>
+                                            Contato: francopasa26@gmail.com<br/>                                    
                                         </p>
-                                    </div>                                                     
+                                    </div>
+                                    </div>
+                                    
+
+                                    <div class="col-sm-4">
+                                    <div class="description-block">
+                                        <h5 class="description-header">Especialização</h5>
+                                        <span class="description-text">2016</span>
+                                    </div>
+                                    </div>
+                                </div>
+                                </div>
                             </div>
-                        </div>
+                            </div>
+                    </div>
+                    <!-- FIm-->
+
+                    <div class="card bg-danger">   
+                        <div class="col-md-16">
+                            <div class="box box-widget widget-user">
+                                <div class="widget-user-header bg-aqua">
+                                <h3 class="widget-user-username">Franco Pasa</h3>
+                                <h5 class="widget-user-desc">Founder &amp; CEO</h5>
+                                </div>
+                                <div class="widget-user-image">
+                                <img class="img-circle" src="\img\avatar1.jpg" alt="User Avatar">
+                                </div>
+                                <div class="box-footer">
+                                <div class="row">
+                                    <div class="col-sm-4 border-right">
+                                    <div class="description-block">
+                                        <h5 class="description-header">5</h5>
+                                        <span class="description-text">Artigos</span>
+                                    </div>
+                                    </div>
+                                    <div class="col-sm-4 border-right">
+                                    <div class="description-block">
+                                            <h5 class="description-header">Formação</h5>
+                                            <span class="description-text">UFMT - 2015</span>
+                                    </div>
+                                    </div>
+                                    <div class="col-sm-4">
+                                    <div class="description-block">
+                                        <h5 class="description-header">Especialização</h5>
+                                        <span class="description-text">2016</span>
+                                    </div>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
+                            </div>
+                    </div>
+                    <!-- FIm-->
+
+
     </div>
    
   </div>
@@ -161,6 +289,11 @@
      <a href="http://www.agraria.pro.br/ojs-2.4.6/index.php?journal=agraria&page=article&op=view&path%5B%5D=534" >
      Germinação de sementes de Heteropteris tomentosa
     </a>
+  </div>
+  <hr>
+  <div class="card-body">    
+    <iframe class="fw-iframe" src="https://feed.mikle.com/widget/v2/94643/" 
+        width="230px" height="370px" frameborder="0" scrolling="no"></iframe>
   </div>
   <div class="card-footer bg-primary">
   &nbsp 
